31a537a2c762e148cffc6b5798d8a706ab8ddb00
user-synax <usersynax@gmail.com>
2026-05-25 07:36:16 +0530

    feat: add or update app/admin/users/page.js

---
+++ app/admin/users/page.js
@@ -0,0 +1,9 @@
+import AdminUsersClient from "@/components/admin/AdminUsersClient";
+
+export const metadata = {
+  title: "User Management | Admin Panel",
+};
+
+export default function AdminUsersPage() {
+  return <AdminUsersClient />;
+}
