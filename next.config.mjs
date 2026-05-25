548d3d5ddefa9bf13e4b942494f9f0cd28e9ec57
user-synax <usersynax@gmail.com>
2026-05-25 07:35:50 +0530

    feat: add or update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,17 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    optimizePackageImports: ['lucide-react'],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com', // For Google OAuth avatars
+      },
+    ],
+  },
 };
 
 export default nextConfig;
